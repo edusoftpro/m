4afca7f04bb92f06f0638c89e91df54eeb224cf2
Bala <anba@gft.com>
2020-03-07 10:46:18 +0100

    Added few improvments for tests

--- dist/features/step_definitions/actionwords.js
+++ dist/features/step_definitions/actionwords.js
@@ -2,11 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class ActionWords {
     iAmAuthorizedUser() {
+        return 'passed';
     }
-    iCallP1Elements(p1 = "test") {
+    iCallP1Elements(p1) {
         return p1;
     }
     iShouldGetTheListOfElements() {
+        return 'passed';
+    }
+    iCallP1ElementsWithProperP2IdOfSpecificElement(p1, p2) {
+        return p1 + p2;
+    }
+    iShouldGetTheElementOfSpecificP1Id(p1) {
+        return p1;
     }
 }
 exports.ActionWords = ActionWords;
--- dist/features/step_definitions/step_definitions.js
+++ dist/features/step_definitions/step_definitions.js
@@ -17,17 +17,17 @@ cucumber_1.Before(() => __awaiter(void 0, void 0, void 0, function* () {
 }));
 cucumber_1.After(() => __awaiter(void 0, void 0, void 0, function* () {
 }));
-cucumber_1.Given('I am authorized user', function () {
-    actionWords.iAmAuthorizedUser();
-    return 'passed';
-});
-cucumber_1.When('I call {string} = elements', function (string) {
-    /* eslint-disable no-console */
-    console.log('string: ' + string);
-    return 'passed';
-    /* eslint-enable no-console */
-});
-cucumber_1.Then('I should get the list of elements', function () {
-    return 'passed';
-});
+cucumber_1.Given(/^I am authorized user$/, () => __awaiter(void 0, void 0, void 0, function* () {
+    return actionWords.iAmAuthorizedUser();
+}));
+cucumber_1.When(/^I call "(.*)" = elements$/, (_) => __awaiter(void 0, void 0, void 0, function* () { return actionWords.iCallP1Elements('passed'); }));
+cucumber_1.Then(/^I should get the list of elements$/, () => __awaiter(void 0, void 0, void 0, function* () {
+    return actionWords.iShouldGetTheListOfElements();
+}));
+cucumber_1.When(/^I call "(.*)" = elements with proper "(.*)" = id of specific element$/, (_, __) => __awaiter(void 0, void 0, void 0, function* () {
+    return actionWords.iCallP1ElementsWithProperP2IdOfSpecificElement('pas', 'sed');
+}));
+cucumber_1.Then(/^I should get the element of specific "(.*)" = id$/, (_) => __awaiter(void 0, void 0, void 0, function* () {
+    return actionWords.iShouldGetTheElementOfSpecificP1Id('passed');
+}));
 //# sourceMappingURL=step_definitions.js.map
